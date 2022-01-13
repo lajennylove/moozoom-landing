a953f613188a94ef8d3ea3befbb198b10244418c
Jenny Martinez <me@lajennylove.com>
2022-01-13 09:22:25 -0500

    Initial commit
    Landing page for moozoom, initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ['./**/*.php', './**/*.html', ],
+	theme: {
+    extend: {
+      backgroundImage: {
+        'gradiente-mobile': "linear-gradient(81deg, rgba(191, 120, 219, 0.942), rgba(54, 150, 205, 0.7653))",
+        'gradiente-footer': "linear-gradient(87.15deg, rgba(54, 150, 205, 0.99), rgba(191, 120, 219, 0.98), rgba(244, 120, 45, 0.98))",
+      }
+    },
+	},
+	variants: {},
+	plugins: [],
+}
